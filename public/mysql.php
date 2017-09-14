fd2365f15851f2ecf2be37ba694350aa6b0b1421
Sangheon Han <han@sangheon.com>
2017-09-14 10:01:39 +0900

    외부 디렉토리의 프로젝트를 지원하도록 설정값 변경

--- public/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$pdo = new \PDO('mysql:host=mysql;dbname=test', 'test', 'test');
-foreach ($pdo->query('SELECT NOW() AS now', \PDO::FETCH_ASSOC) as $row) {
-    var_dump($row);
-}
-$pdo = null;
